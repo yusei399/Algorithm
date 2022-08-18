8f6d78dde3ef426eb023911ab81085fb3a132d79
yusei399 <yusei2021@keio.jp>
2022-08-19 01:51:01 +0900

    remove追加

--- tree/BinaryTree.py
+++ tree/BinaryTree.py
@@ -1,7 +1,3 @@
-from calendar import c
-from importlib.machinery import all_suffixes
-
-
 class Node(object):
 	def __init__(self, value: int) -> None:
 		self.value = value
@@ -19,11 +15,63 @@ def insert(node: Node, value: int) -> Node:
 		node.right = insert(node.right, value)
 	return node
 
+def inorder(node :None) -> None:
+	if node is not None:
+		inorder(node.left)
+		print(node.value)
+		inorder(node.right)
+
+def search(node: Node, value: int) -> bool:
+	if node is None:
+		return False
+
+	if node.value == value:
+		return True
+	elif node.value > value:
+		return search(node.left, value)
+	elif node.value < value:
+		return search(node.right, value)
+
+def min_value(node: None) -> None:
+	current = node
+	while current.left is not None:
+		current = current.left
+	return current
+
+def remove(node: Node, value: int) -> None:
+	if node is None:
+		return node
+	if value < node.value:
+		remove(node.left, value)
+	elif value > node.value:
+		node.right = remove(node.right, value)
+	else:
+		if node.left is None:
+			return node.right
+		elif node.right is None:
+			return node.left
+		
+		temp = min_value(node.right)
+		node.value = temp.value
+		node.right = remove(node.right, temp.value)
+	return node
+
+
 
 if __name__ == '__main__':
 	root = None
 	root = insert(root, 3)
 	root = insert(root, 6)
 	root = insert(root, 5)
-	print(root.value)
-	print(root.right.value)
+	root = insert(root, 7)
+	root = insert(root, 1)
+	root = insert(root, 10)
+	root = insert(root, 2)
+	inorder(root)
+	root = remove(root, 6)
+	inorder(root)
+
+	print(search(root, 1))
+	print(search(root, 4))
+	# print(root.value)
+	# print(root.right.value)
